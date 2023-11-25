076814d2e7de75d16c28be6c121ed9772875d363
AxelSeanCP <104364854+AxelSeanCP@users.noreply.github.com>
2023-11-25 23:16:54 +0700

    update project
    - able to save new user
    - able to return all user in /api/users

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Exercise Tracker
 
 This is the boilerplate for the Exercise Tracker project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker
+
+# How To Use
+<h2>Add new User: </h2>
+<li>put the username in the  <b>create a new username</b> box and press <b>Submit</b></li>
+<li>it will show you the username and id, copy the id somewhere because the create a new username will generate new id</li>
+
+<h2>Add exercises: </h2>
+<li></li>
+
+<h2>In the URL</h2>
+<li>use <b>/api/users</b> to get all users</li>
--- index.js
+++ index.js
@@ -1,15 +1,89 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+// connect to database
+mongoose.connect(process.env.MONGO_URI, { useNewURLParser: true, useUnifiedTopology: true });
+
+// check connection to database
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, "connection error:"));
+connection.on('open', () => {
+  console.log("Connected to the database")
+});
+
+// define user model
+const logSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  }
+}, {_id: false});
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  logs: [logSchema]
+});
+
+let User = mongoose.model('User', userSchema);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+app.post('/api/users', (req,res) => {
+  const userBaru = new User({
+    username: req.body.username
+  });
+
+  userBaru.save().then(() => {
+    console.log("New user added successfully!");
+    res.json({username: req.body.username, _id: userBaru._id});
+  }).catch((err) => {
+    console.error(err,"Failed to add new user!");
+    res.json({error: "failed to add user"});
+  });
+});
+
+app.get('/api/users', async (req,res) => {
+  try {
+    
+    const database = [];
+
+    for await (const user of User.find()){
+      const data = {
+        username: user.username,
+        _id: user._id
+      }
+  
+      database.push(data);
+    }
+
+    res.send(database);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'internal server error'});
+  }
+});
 
 
 
